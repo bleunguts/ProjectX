8f78293a7fea1f5f9045ba0ce545e59b107a44fc
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2023-09-29 00:26:58 +0100

    Branded the shell

--- Shell/ShellViewModel.cs
+++ Shell/ShellViewModel.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Shell
 {
@@ -14,7 +15,7 @@ namespace Shell
     public class ShellViewModel : Conductor<IScreen>.Collection.OneActive, IConductor, IHandle<ModelEvents>
     {
         #region Bindable Properties       
-        private string statusText = string.Empty;
+        private string statusText = "Ready.";
 
         public string StatusText
         {
@@ -36,7 +37,6 @@ namespace Shell
             this.events.SubscribeOnPublishedThread(this);
             DisplayName = "ProjectX";
         }
-
         public Task HandleAsync(ModelEvents message, CancellationToken cancellationToken)
         {
             return Task.Run(() =>
@@ -44,6 +44,30 @@ namespace Shell
                 var events = message.EventList;
                 StatusText = events.First().ToString();
             }, cancellationToken);
+        }
+        public void OnClick(object sender)
+        {
+            Button btn = sender as Button;
+            int num = Convert.ToInt16(btn.Content.ToString().Split(' ')[1]);
+            string ch = "Ch";
+            if (num < 10)
+                ch += "0" + num.ToString();
+            else
+                ch += num.ToString();
+
+            Items.Clear();
+            var sc = from q in screens where q.ToString().Contains(ch) orderby q.DisplayName select q;
+            Items.AddRange(sc);
+
+            var view = this.GetView() as ShellView;
+            foreach (Button b in view.buttonPanel.Children)
+            {
+                b.Background = Brushes.Transparent;
+                b.Foreground = Brushes.Black;
+            }
+
+            btn.Background = Brushes.Black;
+            btn.Foreground = Brushes.White;
         }
     }
 }
