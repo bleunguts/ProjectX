0c5326b92e213b0d246d1c810591bf7402717084
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2023-10-05 02:45:35 +0100

    Added React Web component that starts up a templated page

---
+++ Web/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "./Dashboard"
+import Dashboard from './Dashboard'
+
+function App() {  
+
+  return (
+      <>            
+        
+        <Dashboard/>      
+    </>
+  )
+}
+
+export default App
