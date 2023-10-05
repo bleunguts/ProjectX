fda594f6856aa9ea76771752b100e69f94f4025c
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2023-10-05 13:35:30 +0100

    Added more blog information

--- Web/src/components/layout2/Blog.tsx
+++ Web/src/components/layout2/Blog.tsx
@@ -19,15 +19,26 @@ const sections = [
 const mainFeaturedPost = {
   title: 'Innovative trading signals engineering platform',
   description:
-        "Trading strategies employed mainly consists of mean reversion, momentum, arbitrage strategies. This app allows conceptualization of trading strategies and offers a backtesting platform to simulate the strategy based on historical market sourced prices",
+        "The purpose of Trading Signals is provide a platform for coming up with trading strategies, and being able to exercise back testing on the strategies for personal stock investments.  My ethos is simple, easy to use, and without clutter effective tooling for small scale personal home trading.",
   image: 'https://source.unsplash.com/random?wallpapers',
   imageText: 'main image description',  
 };
 
 const featuredPosts = [
   {
+        title: 'Financial Quant Development',
+        date: 'Jul 23',
+        description:
+            `As the main purpose is to design a trading signals app when I was looking at my personal trading books on HSBC, 
+                it also acts as a show case in my keen interest in financial quant development in C#/C++, a decade of experience in risk management systems and trading execution.
+                Employing cutting edge tech stack for a scalable solution and cloud ready C# 11,.NET 7,WPF,Caliburn.Micro,ComponentModel.CompositionContainer (IoC), AspNet Core 6, ReactiveX, Background Services.
+                I wrote the Quant models in c# (black-scholes, barrier options, bonds pricing, CDS pricing) and some options C++ (bs, pseudo random generators)`,
+        image: './signals.PNG',
+        imageLabel: 'Image Text',
+  },
+  {
     title: 'Mean Reversion Trading signals',
-    date: 'Nov 12',
+    date: 'Sep 23',
     description:
       'The main purpose is to generate trading signals such as mean reversion and provide backtesting',
     image: './signals.PNG',
@@ -35,12 +46,21 @@ const featuredPosts = [
   },
   {
     title: 'Arbitrage Trading signals',
-    date: 'Nov 11',
+    date: 'Oct 23',
     description:
-      'This is a more involved component of the system. The app provides various pricers that I have developed for options, fixed income, FX products and will show actual market prices to compare with.  This provides analytics tools on mismatches in function inputs such as (vol, spot prices) that can expose potential arbitrage ops',
+          `I have come up with my own Arbitrage Trading signals which is quite complex. The app provides various pricers that I have developed for options, fixed income, FX products and will show actual market prices to compare with.  
+            This provides analytics tools on mismatches in function inputs such as (vol, spot prices) that can expose potential arbitrage ops`,
       image: './signals.PNG',
     imageLabel: 'Image Text',
-  },
+    },
+    {
+        title: 'Market Data',
+        date: 'Oct 23',
+        description:
+            `As I have been using Quandl to source market data over the internet, I found a great api which provides not only stock information but financial summaries and economic news.  I will be trialing this, stay tuned!`,
+        image: './signals.PNG',
+        imageLabel: 'Image Text',
+    },
 ];
 
 const sidebar = {
@@ -78,7 +98,7 @@ export default function Blog() {
         </main>
       </Container>
       <Footer
-        title="Footer"
+        title=""
         description="Think,Change,Do."
       />
     </ThemeProvider>
