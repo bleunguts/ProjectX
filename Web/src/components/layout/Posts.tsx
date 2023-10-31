dc4104c89be5d5d1cf248bbf4b69ce542ed32f77
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2023-10-31 21:47:38 +0000

    Reduce stuff so that it fits

--- Web/src/components/layout/Posts.tsx
+++ Web/src/components/layout/Posts.tsx
@@ -3,8 +3,7 @@ export const featuredPosts = [
         title: 'The financial quant development journey..',
         date: 'Jul 23',
         description:
-            `It came about when I wanted to design trading signals app using industry APIs (NinjaTrader API, Skender API) and wanted a simple, clutter free stock price indicator without the overwhelm of sophisticated bells and whistles.
-            This acts as a engineering showcase that demonstrates the application of financial programming utilizing cloud-ready latest tech stack.
+            `It came about when I wanted to design trading signals app using industry APIs (NinjaTrader API, Skender API) cloud-ready latest tech stack.
             `,
         image: './signals.PNG',
         imageLabel: 'Image Text',
@@ -13,7 +12,7 @@ export const featuredPosts = [
         title: 'The roadmap ahead..',
         date: 'Sep 23',
         description:
-            `Future developments include: integrating more complex NinjaTrader stock trend indicator to support momentum strategies (Relative Strength Index, Williams %R).  Support for pairs trading a steady strateggy to hedge sector and market risk going long on under-performer while going short on over performer when spread is away from mean. 
+            `Implement complex NinjaTrader support momentum strategies(RSI, Williams %R) and Pairs strategy going long on under-performer short on over performer. 
             `,
         image: './signals.PNG',
         imageLabel: 'Image Text',
