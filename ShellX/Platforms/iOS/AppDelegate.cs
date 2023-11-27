1042dce5e320fd1515195c43a53b3e65f5376675
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2023-11-27 02:35:48 +0000

    Initial .NET MAUI checkin

---
+++ ShellX/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShellX
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
