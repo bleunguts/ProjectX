0ff9d6a09387d66661542c0bd0a4441f685512e5
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2023-11-28 05:37:31 +0000

    Workaround to copy dependent dll for github workflow

---
+++ ProjectX.AnalyticsLibNative/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Dev/projects/GitHub/ProjectX/ProjectX.AnalyticsLibNative
+# Build directory: C:/Dev/projects/GitHub/ProjectX/ProjectX.AnalyticsLibNative/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
