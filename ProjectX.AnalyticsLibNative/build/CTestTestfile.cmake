9604dd2300006c2a6fc7249a5dfe565a72bf49e5
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2023-11-28 05:48:08 +0000

    Remove accidental checkins

--- ProjectX.AnalyticsLibNative/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/projects/GitHub/ProjectX/ProjectX.AnalyticsLibNative
-# Build directory: C:/Dev/projects/GitHub/ProjectX/ProjectX.AnalyticsLibNative/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- ProjectX.AnalyticsLibNative/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/projects/GitHub/ProjectX/ProjectX.AnalyticsLibNative/src
-# Build directory: C:/Dev/projects/GitHub/ProjectX/ProjectX.AnalyticsLibNative/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
