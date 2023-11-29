277bc758b6ce3d87e15630754ba80b8211990b46
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2023-11-29 23:02:38 +0000

    upgrade node tools

--- Web/src/components/layout/Footer.tsx
+++ Web/src/components/layout/Footer.tsx
@@ -1,5 +1,4 @@
 import {useEffect, useState} from 'react';
-import axios from 'axios';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
