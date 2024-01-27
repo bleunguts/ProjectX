43bbca33b80ce8f9880558fb4947aa7222dd2539
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2024-01-27 11:17:02 +0000

    Revert to cached data if server is returning 500 too many requests

--- Web/src/TradingStrategyStore.ts
+++ Web/src/TradingStrategyStore.ts
@@ -1,7 +1,7 @@
 import { RootStore } from "./RootStore";
 import { BackenedApi } from "./api";
 import { reaction, runInAction, makeAutoObservable } from 'mobx';
-import { FakeStrategyPlaceholder } from "./components/layout/DummyData";
+import { FakeStrategyBTCUSD as FakeStrategyBTCUSData, FakeStrategyChartData, FakeStrategyIBMData, FakeStrategyPlaceholder } from "./components/layout/DummyData";
 import { AxiosResponse } from "axios";
 
 export interface ChartData {
@@ -42,8 +42,15 @@ export class TradingStrategyStore
                     .then((res) => this.data = ((res as AxiosResponse<never, never>).data as ChartData[]))
                     .catch((e) => {
                         console.log(`Error occurred whilst loading new symbol... ${e}`);
-                        this.symbol = `ERROR LOADING SYMBOL..`;
-                        this.data = FakeStrategyPlaceholder;
+                        console.log(e);
+                        const cache = cachedData(symbol);
+                        if (cache != undefined){
+                            this.data = cache as ChartData[];
+
+                        } else {
+                            this.symbol = `ERROR LOADING SYMBOL..`;
+                            this.data = FakeStrategyPlaceholder;
+                        }
                     });
                 
                 console.log(`Symbol: ${symbol} loaded, data length: ${this.data.length}`);
@@ -60,13 +67,13 @@ export class TradingStrategyStore
     }
 }
 
-// function fetchData(symbol: string): ChartData[] {
-//     switch(symbol)
-//     {
-//         case 'AAPL': return FakeStrategyChartData;
-//         case 'IBM' : return FakeStrategyIBMData;
-//         case 'BTCUSD' : return FakeStrategyBTCUSDData;
-//         default: throw new Error(`Cannot load data for symbol ${symbol}`);
-//     }
-// }
+function cachedData(symbol: string): ChartData[] | undefined  {
+    switch(symbol)
+    {
+        case 'AAPL': return FakeStrategyChartData;
+        case 'IBM' : return FakeStrategyIBMData;
+        case 'BTCUSD' : return FakeStrategyBTCUSData;
+        default: return undefined;
+    }
+}
 
