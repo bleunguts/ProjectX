c2062622dcdc6ee70ca3df28da97f49eb69dc7c2
Billy Leung <82518709+bleunguts@users.noreply.github.com>
2025-01-09 10:39:05 +0000

    Upgrade projects to .NET 9.0 including azure cloud deployments

--- ProjectX.AnalyticsCppLib/AssemblyInfo.cpp
+++ ProjectX.AnalyticsCppLib/AssemblyInfo.cpp
@@ -11,7 +11,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"")];
 [assembly:AssemblyProductAttribute(L"ProjectXAnalyticsCppLib")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2023")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2025")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
